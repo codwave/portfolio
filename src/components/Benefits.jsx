522b727a3b3a565db62a3214912b89644d87196f
jiya1724 <jiyatrivedi204@gmail.com>
2024-07-17 00:36:41 +0530

    refact:benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -23,19 +23,14 @@ const Benefits = () => {
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
+                <div className="flex items-center justify-end p-2 mt-auto">
                   <img
                     src={item.iconUrl}
                     width={48}
                     height={48}
                     alt={item.title}
                   />
-                  <a href="#contact" className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </a>
-                  <a href="#contact">
-                    <Arrow />
-                  </a>
+                  
                 </div>
               </div>
               
